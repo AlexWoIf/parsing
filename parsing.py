0dbddf61a5f79581e3725c10f1a675249b9a828e
AlexWoIf <alexey.barannikov@gmail.com>
2023-12-04 12:30:23 +0300

    download book covers

--- parsing.py
+++ parsing.py
@@ -3,30 +3,44 @@ import os
 from pathlib import Path
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin, urlsplit
 
 
 BOOK_DIR = 'books/'
-DOWNLOAD_URL = 'https://tululu.org/txt.php?id='
-POST_URL = 'https://tululu.org/b'
+IMAGE_DIR = 'images/'
+SITE_URL = 'https://tululu.org/'
+DOWNLOAD_URL = '/txt.php?id='
+POST_URL = '/b'
 TITLE_DELIMITER = ' \xa0 :: \xa0 '
 
 
 def check_for_redirect(response):
-    print(response.history)
     if response.history:
         raise requests.exceptions.HTTPError
 
 
-def parse_page_by_id(id):
-    res = requests.get(url=f'{POST_URL}{id}/')
-    print(f'{POST_URL}{id}', res)
+def parse_post_page(html):
+    soup = BeautifulSoup(html, 'lxml')
+    file_link = soup.find('a', string='скачать txt')
+    if not file_link:
+        return
+    file_url = file_link['href']
+    title = file_link['title'][:-20]
+    img_src = soup.find('div', {'class': 'bookimage'}).find('img')['src']
+    return {'file_url': file_url,
+            'title': title,
+            'img_src': img_src, }
+
+
+def get_post_page(url):
+    res = requests.get(url)
     res.raise_for_status()
+    print(res.history)
     check_for_redirect(res)
-    soup = BeautifulSoup(res.text, 'lxml')
-    return soup.find('h1').text.split(TITLE_DELIMITER)
+    return res
 
 
-def get_book_by_url(url):
+def get_book_file(url):
     res = requests.get(url)
     res.raise_for_status()
     check_for_redirect(res)
@@ -34,8 +48,24 @@ def get_book_by_url(url):
 
 
 def download_txt(url, filename, folder=BOOK_DIR):
-    res = get_book_by_url(url)
-    filename = re.sub(r'[^\w ]', '', filename)+'.txt'
+    res = get_book_file(url)
+    filename = re.sub(r'[^\w\d\. ]', '', filename) + '.txt'
+    filepath = os.path.join(folder, filename)
+    with open(filepath, 'wb') as file:
+        file.write(res.content)
+    return filepath
+
+
+def download_image(url, filename, folder=BOOK_DIR):
+    res = requests.get(url)
+    res.raise_for_status()
+    check_for_redirect(res)
+    name = url.split('/')[-1]
+    if filename:
+        ext = (name+'.').split('.')[1]
+        filename = ''.join(re.sub(r'[^\w\d\. ]', '', filename), '.', ext)
+    else:
+        filename = name
     filepath = os.path.join(folder, filename)
     with open(filepath, 'wb') as file:
         file.write(res.content)
@@ -44,13 +74,18 @@ def download_txt(url, filename, folder=BOOK_DIR):
 
 if __name__ == "__main__":
     Path(BOOK_DIR).mkdir(parents=True, exist_ok=True)
+    Path(IMAGE_DIR).mkdir(parents=True, exist_ok=True)
     for id in range(1, 11):
-        print(id)
         try:
-            title, _ = parse_page_by_id(id)
-            print(title)
-            download_txt(f'{DOWNLOAD_URL}{id}', title)
-            print(parse_page_by_id(id))
+            res = get_post_page(urljoin(SITE_URL, f'{POST_URL}{id}/'))
+            page_data = parse_post_page(res.text)
+            if not page_data:
+                print('not found')
+                continue
+            print(page_data)
+            file_url, title, img_url = page_data.values()
+            download_txt(urljoin(SITE_URL, file_url), f'{id}. {title}')
+            download_image(urljoin(SITE_URL, img_url), '', IMAGE_DIR)
         except requests.exceptions.HTTPError:
-            print('error')
+            print('HTTP error')
             continue
